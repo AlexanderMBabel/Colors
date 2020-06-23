91153cd72f2d6405cfc73027ffd7f0628738898a
AlexanderMBabel <alexandermbabel@protonmail.com>
2020-06-23 14:00:12 -0400

    added Palette route

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,26 @@ import seedColors from './seedColors';
 import { generatePalette } from './utils/colorHelpers';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find((palette) => palette.id === id);
+  }
   render() {
     return (
       <div className='App'>
         <Switch>
           <Route exact path='/' />
-          <Route exact path='/palette/:id' />
+          <Route
+            exact
+            path='/palette/:id'
+            render={(routeProps) => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
         </Switch>
-        {/* <Palette palette={generatePalette(seedColors[0])} /> */}
       </div>
     );
   }
